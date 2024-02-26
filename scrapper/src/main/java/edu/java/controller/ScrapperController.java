d124ffc2f94c1e542609074271c7b4ceed0ed22a
Mark Solonchev <solonchev32@gmail.com>
2024-02-26 11:30:51 +0300

    add deleteChat in ScrapperController

--- scrapper/src/main/java/edu/java/controller/ScrapperController.java
+++ scrapper/src/main/java/edu/java/controller/ScrapperController.java
@@ -3,6 +3,7 @@ package edu.java.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,14 @@ public class ScrapperController {
         @PathVariable Long id
     ) {
         log.info("Чат зарегистрирован");
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/tg-chat/{id}")
+    public ResponseEntity<Void> deleteChat(
+        @PathVariable Long id
+    ) {
+        log.info("Чат успешно удален");
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
