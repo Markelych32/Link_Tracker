49fe91e893da892fed8fa14e6604153ff81e149b
Mark Solonchev <solonchev32@gmail.com>
2024-02-26 11:41:13 +0300

    add addLinks in ScrapperController

--- scrapper/src/main/java/edu/java/controller/ScrapperController.java
+++ scrapper/src/main/java/edu/java/controller/ScrapperController.java
@@ -1,5 +1,7 @@
 package edu.java.controller;
 
+import edu.java.controller.dto.AddLinkRequest;
+import edu.java.controller.dto.LinkResponse;
 import edu.java.controller.dto.ListLinksResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +42,15 @@ public class ScrapperController {
     ) {
         log.info("Ссылки успешно получены");
         return new ResponseEntity<>(new ListLinksResponse(), HttpStatus.OK);
+    }
+
+    @PostMapping("/links")
+    public ResponseEntity<LinkResponse> addLinks(
+        @RequestHeader("Tg-Chat-Id") Long chatId,
+        @RequestBody AddLinkRequest addLinkRequest
+    ) {
+        log.info("Ссылка успешно добавлена");
+        return new ResponseEntity<>(new LinkResponse(chatId, addLinkRequest.getUrl()), HttpStatus.OK);
     }
 
 }
