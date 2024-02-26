540271c995650b3b9a356dd3dde7884b3e68bd6e
Mark Solonchev <solonchev32@gmail.com>
2024-02-26 11:35:48 +0300

    add getLinks in ScrapperController

--- scrapper/src/main/java/edu/java/controller/ScrapperController.java
+++ scrapper/src/main/java/edu/java/controller/ScrapperController.java
@@ -1,11 +1,14 @@
 package edu.java.controller;
 
+import edu.java.controller.dto.ListLinksResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +31,14 @@ public class ScrapperController {
     ) {
         log.info("Чат успешно удален");
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/links")
+    public ResponseEntity<ListLinksResponse> getLinks(
+        @RequestHeader("Tg-Chat-Id") Long chatId
+    ) {
+        log.info("Ссылки успешно получены");
+        return new ResponseEntity<>(new ListLinksResponse(), HttpStatus.OK);
     }
 
 }
