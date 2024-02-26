0bcba752e5a9b09be34bb3637fe92e9b0374d9ff
Mark Solonchev <solonchev32@gmail.com>
2024-02-26 11:18:37 +0300

    add registerChat in ScrapperController

---
+++ scrapper/src/main/java/edu/java/controller/ScrapperController.java
@@ -0,0 +1,24 @@
+package edu.java.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/scrapper-api/v1.0")
+@Slf4j
+public class ScrapperController {
+
+    @PostMapping("/tg-chat/{id}")
+    public ResponseEntity<Void> registerChat(
+        @PathVariable Long id
+    ) {
+        log.info("Чат зарегистрирован");
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
