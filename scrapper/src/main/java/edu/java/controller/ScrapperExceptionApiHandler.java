7561b46e52ef6cf6b04a34c0777d54c3c2179da9
Mark Solonchev <solonchev32@gmail.com>
2024-02-27 15:00:22 +0300

    add API Handler for LinkNotFoundByUrlException

--- scrapper/src/main/java/edu/java/controller/ScrapperExceptionApiHandler.java
+++ scrapper/src/main/java/edu/java/controller/ScrapperExceptionApiHandler.java
@@ -4,6 +4,7 @@ import edu.java.controller.dto.ApiErrorResponse;
 import edu.java.exception.ChatAlreadyExistException;
 import edu.java.exception.ChatNotFoundException;
 import edu.java.exception.LinkAlreadyRegisteredInChatException;
+import edu.java.exception.LinkNotFoundByUrlException;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -67,6 +68,19 @@ public class ScrapperExceptionApiHandler {
                 .code("404")
                 .exceptionName("LinkAlreadyRegisteredInChatException")
                 .exceptionMessage("Link was already tracked by chat")
+                .stacktrace(list)
+                .build(), HttpStatusCode.valueOf(404)
+        );
+    }
+    @ExceptionHandler(LinkNotFoundByUrlException.class)
+    public ResponseEntity<ApiErrorResponse> linkNotFoundByUrl(Exception exception) {
+        List<String> list = Arrays.stream(exception.getStackTrace()).map(StackTraceElement::toString).toList();
+        return new ResponseEntity<>(
+            ApiErrorResponse.builder()
+                .description("Link was not found with such url")
+                .code("404")
+                .exceptionName("LinkNotFoundByUrlException")
+                .exceptionMessage("Link was not found with such url")
                 .stacktrace(list)
                 .build(), HttpStatusCode.valueOf(404)
         );
