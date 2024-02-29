5cf90d4825afd1f8e26983742e2b33b68e7c89e7
Mark Solonchev <solonchev32@gmail.com>
2024-02-29 17:20:48 +0300

    make clients as beans without configuration

--- scrapper/src/main/java/edu/java/github/GithubClient.java
+++ scrapper/src/main/java/edu/java/github/GithubClient.java
@@ -1,11 +1,15 @@
 package edu.java.github;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Component
 public class GithubClient {
     private final WebClient webClient;
     private static final String BASE_URL = "https://api.github.com";
 
+    @Autowired
     public GithubClient(WebClient.Builder webClient) {
         this.webClient = webClient.baseUrl(BASE_URL).build();
     }
