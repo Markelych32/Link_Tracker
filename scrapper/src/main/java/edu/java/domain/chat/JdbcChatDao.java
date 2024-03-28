f662581dc5014ffc4c785e6f6573f6d739c4261e
Mark Solonchev <solonchev32@gmail.com>
2024-03-28 19:03:20 +0300

    add namedParameterJdbcTemplate in ChatDao

--- scrapper/src/main/java/edu/java/domain/chat/JdbcChatDao.java
+++ scrapper/src/main/java/edu/java/domain/chat/JdbcChatDao.java
@@ -6,7 +6,9 @@ import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,33 +16,40 @@ import org.springframework.stereotype.Repository;
 @Slf4j
 public class JdbcChatDao implements ChatDao {
 
-    private static final String ADD_SQL = "INSERT INTO chat(id) VALUES (?)";
-    private static final String REMOVE_SQL = "DELETE FROM chat WHERE id = ?";
+    private static final String ADD_SQL = "INSERT INTO chat(id) VALUES (:id)";
+    private static final String REMOVE_SQL = "DELETE FROM chat WHERE id = :id";
     private static final String FIND_ALL_SQL = "SELECT id FROM chat";
-    private static final String FIND_SQL = "SELECT id FROM chat WHERE id = ?";
+    private static final String FIND_SQL = "SELECT id FROM chat WHERE id = :id";
 
-    private final JdbcTemplate jdbcTemplate;
     private final ChatMapper chatRowMapper;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Override
     public boolean add(Long tgChatId) {
-        return jdbcTemplate.update(ADD_SQL, tgChatId) > 0;
+        SqlParameterSource namedParameters = new MapSqlParameterSource("id", tgChatId);
+        return namedParameterJdbcTemplate.update(ADD_SQL, namedParameters) > 0;
     }
 
     @Override
     public boolean remove(Long tgChatId) {
-        return jdbcTemplate.update(REMOVE_SQL, tgChatId) > 0;
+        SqlParameterSource namedParameters = new MapSqlParameterSource("id", tgChatId);
+        return namedParameterJdbcTemplate.update(REMOVE_SQL, namedParameters) > 0;
     }
 
     @Override
     public List<Chat> findAll() {
-        return jdbcTemplate.query(FIND_ALL_SQL, chatRowMapper);
+        return namedParameterJdbcTemplate.query(FIND_ALL_SQL, chatRowMapper);
     }
 
     @Override
     public Optional<Chat> find(Long tgChatId) {
         try {
-            return Optional.ofNullable(jdbcTemplate.queryForObject(FIND_SQL, chatRowMapper, tgChatId));
+            SqlParameterSource namedParameters = new MapSqlParameterSource("id", tgChatId);
+            return Optional.ofNullable(namedParameterJdbcTemplate.queryForObject(
+                FIND_SQL,
+                namedParameters,
+                chatRowMapper
+            ));
         } catch (EmptyResultDataAccessException e) {
             return Optional.empty();
         }
