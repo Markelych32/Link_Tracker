11462e5968bea34549e161fc7faf5ee1f375cdff
Mark Solonchev <solonchev32@gmail.com>
2024-04-04 19:42:44 +0300

    fix rate limit for bot controller

--- bot/src/main/java/edu/java/bot/controller/BotController.java
+++ bot/src/main/java/edu/java/bot/controller/BotController.java
@@ -1,46 +1,38 @@
 package edu.java.bot.controller;
 
-import edu.java.bot.configuration.RateConfiguration;
 import edu.java.bot.controller.dto.request.LinkUpdate;
 import edu.java.bot.processor.BotMessageSender;
-import io.github.bucket4j.Bandwidth;
+import edu.java.bot.service.RateLimitService;
 import io.github.bucket4j.Bucket;
-import io.github.bucket4j.Refill;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
-import java.time.Duration;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequiredArgsConstructor
 @Slf4j
 public class BotController {
 
-    private final BotMessageSender botMessageSender;
-    private final Bucket bucket;
+    private static final String X_FORWARDED_FOR = "X-Forwarded-For";
 
-    @Autowired
-    public BotController(BotMessageSender botMessageSender, RateConfiguration rateConfiguration) {
-        this.botMessageSender = botMessageSender;
-        Bandwidth limit = Bandwidth.classic(
-            rateConfiguration.limit(),
-            Refill.intervally(rateConfiguration.refill(), Duration.ofMinutes(1))
-        );
-        this.bucket = Bucket.builder()
-            .addLimit(limit)
-            .build();
-    }
+    private final BotMessageSender botMessageSender;
+    private final RateLimitService rateLimitService;
 
     @PostMapping("/updates")
     public ResponseEntity<Void> sendUpdate(
-        @Valid @RequestBody LinkUpdate linkUpdate
+        @Valid @RequestBody LinkUpdate linkUpdate,
+        HttpServletRequest request
     ) {
+        final String ip = Optional.ofNullable(request.getHeader(X_FORWARDED_FOR))
+            .orElseGet(request::getRemoteAddr);
+        Bucket bucket = rateLimitService.catchBucket(ip);
         if (bucket.tryConsume(1)) {
             botMessageSender.sendMessage(
                 linkUpdate.getTgChatIds(),
