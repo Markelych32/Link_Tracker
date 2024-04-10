e7cd17413e38847587948b0bd1f4af241a85120a
Mark Solonchev <solonchev32@gmail.com>
2024-04-11 00:16:55 +0300

    add Kafka Producer at Bot

--- bot/src/main/java/edu/java/bot/BotApplication.java
+++ bot/src/main/java/edu/java/bot/BotApplication.java
@@ -3,6 +3,7 @@ package edu.java.bot;
 import edu.java.bot.configuration.ApplicationConfig;
 import edu.java.bot.configuration.ClientConfig;
 import edu.java.bot.configuration.kafka.properties.KafkaConsumer;
+import edu.java.bot.configuration.kafka.properties.KafkaProducer;
 import edu.java.bot.configuration.retryConfig.RetryConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +15,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
     ApplicationConfig.class,
     ClientConfig.class,
     RetryConfig.class,
-    KafkaConsumer.class
+    KafkaConsumer.class,
+    KafkaProducer.class
 })
 @RequiredArgsConstructor
 public class BotApplication {
